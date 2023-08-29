cad819d0f9db717eae00207e76e8ad9707069e55
Burak Şenses <buraksenseees@gmail.com>
2023-08-29 20:27:09 +0300

    backend initialized

---
+++ 01-backend/src/main/java/com/sirhot/usePopcorn/Demo/Project/UsePopcornDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sirhot.usePopcorn.Demo.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsePopcornDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsePopcornDemoProjectApplication.class, args);
+	}
+
+}
---
+++ 01-backend/src/test/java/com/sirhot/usePopcorn/Demo/Project/UsePopcornDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirhot.usePopcorn.Demo.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsePopcornDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
